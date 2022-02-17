4d34a44446979764e4a3f23f6c5558db7c8fa4d1
W47K3R <99557100+walker79223@users.noreply.github.com>
2022-02-17 17:58:36 +0530

    Add files via upload

---
+++ alan walker.py
@@ -0,0 +1,11 @@
+import pyautogui    
+import time
+time.sleep(2)
+pyautogui.dragRel(250, -500, duration=1)
+pyautogui.dragRel(250, 575, duration=1)
+pyautogui.moveRel(0, -650, duration=1)
+pyautogui.moveRel(-575, 0, duration=1)
+pyautogui.dragRel(200, 650, duration=1)
+pyautogui.dragRel(110, -250, duration=1)
+pyautogui.dragRel(110, 250, duration=1)
+pyautogui.dragRel(200, -600, duration=1)
